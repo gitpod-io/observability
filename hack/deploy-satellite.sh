8eada6a501623ea499564c9d714f60082e48fc5f
Arthur Silva Sens <arthursens2005@gmail.com>
2021-11-05 18:11:40 +0000

    hack/deploy-satellite: Replace apply with create
    Signed-off-by: GitHub <noreply@github.com>

--- hack/deploy-satellite.sh
+++ hack/deploy-satellite.sh
@@ -3,14 +3,14 @@
 kubectl apply -f monitoring-satellite/manifests/namespace.yaml
 kubectl apply -f monitoring-satellite/manifests/podsecuritypolicy-restricted.yaml
 
-kubectl apply -f monitoring-satellite/manifests/prometheus-operator/0alertmanagerConfigCustomResourceDefinition.yaml
-kubectl apply -f monitoring-satellite/manifests/prometheus-operator/0alertmanagerCustomResourceDefinition.yaml
-kubectl apply -f monitoring-satellite/manifests/prometheus-operator/0podmonitorCustomResourceDefinition.yaml
-kubectl apply -f monitoring-satellite/manifests/prometheus-operator/0probeCustomResourceDefinition.yaml
-kubectl apply -f monitoring-satellite/manifests/prometheus-operator/0prometheusCustomResourceDefinition.yaml
-kubectl apply -f monitoring-satellite/manifests/prometheus-operator/0prometheusruleCustomResourceDefinition.yaml
-kubectl apply -f monitoring-satellite/manifests/prometheus-operator/0servicemonitorCustomResourceDefinition.yaml
-kubectl apply -f monitoring-satellite/manifests/prometheus-operator/0thanosrulerCustomResourceDefinition.yaml
+kubectl create -f monitoring-satellite/manifests/prometheus-operator/0alertmanagerConfigCustomResourceDefinition.yaml
+kubectl create -f monitoring-satellite/manifests/prometheus-operator/0alertmanagerCustomResourceDefinition.yaml
+kubectl create -f monitoring-satellite/manifests/prometheus-operator/0podmonitorCustomResourceDefinition.yaml
+kubectl create -f monitoring-satellite/manifests/prometheus-operator/0probeCustomResourceDefinition.yaml
+kubectl create -f monitoring-satellite/manifests/prometheus-operator/0prometheusCustomResourceDefinition.yaml
+kubectl create -f monitoring-satellite/manifests/prometheus-operator/0prometheusruleCustomResourceDefinition.yaml
+kubectl create -f monitoring-satellite/manifests/prometheus-operator/0servicemonitorCustomResourceDefinition.yaml
+kubectl create -f monitoring-satellite/manifests/prometheus-operator/0thanosrulerCustomResourceDefinition.yaml
 
 until kubectl get servicemonitors.monitoring.coreos.com --all-namespaces ; do date; sleep 1; echo ""; done
 until kubectl get prometheusrules.monitoring.coreos.com --all-namespaces ; do date; sleep 1; echo ""; done
