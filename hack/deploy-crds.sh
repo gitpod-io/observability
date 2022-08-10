51a64fea2351d3615005934863eaa91b43ed9f6a
ArthurSens <arthursens2005@gmail.com>
2022-08-10 14:41:16 +0000

    Enable e2e-tests for CLI installer
    Signed-off-by: ArthurSens <arthursens2005@gmail.com>

---
+++ hack/deploy-crds.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+KUBECONFIG=""
+KUBECONFIG_FLAG=""
+
+opts=$(getopt \
+  --longoptions "kubeconfig:" \
+  --name "$(basename "$0")" \
+  --options "" \
+  -- "$@"
+)
+
+eval set -- "$opts"
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --kubeconfig) KUBECONFIG=$2 ; shift 2 ;;
+    *) break ;;
+  esac
+done
+
+if [[ $KUBECONFIG != "" ]]; then
+  KUBECONFIG_FLAG="--kubeconfig ${KUBECONFIG}"
+fi
+
+for CRD in $(find monitoring-satellite/manifests/prometheusOperator/ -type f -name "*CustomResourceDefinition.yaml"); 
+do 
+  kubectl $KUBECONFIG_FLAG replace -f $CRD || kubectl $KUBECONFIG_FLAG create -f $CRD
+done
+
+until kubectl $KUBECONFIG_FLAG get servicemonitors.monitoring.coreos.com --all-namespaces ; do date; sleep 1; echo ""; done
+until kubectl $KUBECONFIG_FLAG get prometheusrules.monitoring.coreos.com --all-namespaces ; do date; sleep 1; echo ""; done
+
+kubectl $KUBECONFIG_FLAG apply -f monitoring-satellite/manifests/pyrra/crd.yaml
