7e0c0e49e981a26b4b6bba2134ef08aeb3c17d68
Arthur Silva Sens <arthursens2005@gmail.com>
2022-08-11 18:43:17 +0000

    Fix otel-collector resources
    Signed-off-by: Arthur Silva Sens <arthursens2005@gmail.com>

--- installer/pkg/components/otel-collector/objects.go
+++ installer/pkg/components/otel-collector/objects.go
@@ -1,6 +1,8 @@
 package otelcollector
 
 import (
+	"errors"
+
 	"k8s.io/apimachinery/pkg/runtime"
 
 	"github.com/gitpod-io/observability/installer/pkg/common"
@@ -8,6 +10,13 @@ import (
 
 func Objects(ctx *common.RenderContext) common.RenderFunc {
 	if ctx.Config.Tracing.Install {
+
+		if ctx.Config.Tracing.HoneycombAPIKey == "" || ctx.Config.Tracing.HoneycombDataset == "" {
+			return func(cfg *common.RenderContext) ([]runtime.Object, error) {
+				return []runtime.Object{}, errors.New("'honeycombDataset' and 'honeycombAPIKey' are required when tracing is enabled")
+			}
+		}
+
 		return common.CompositeRenderFunc(
 			clusterRole,
 			clusterRoleBinding,
