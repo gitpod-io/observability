033ed867304a160ffd21b5d535afbd81d4183b65
Arthur Silva Sens <arthursens2005@gmail.com>
2022-08-11 15:09:15 +0000

    Disable rendering certmanager resources by default
    Signed-off-by: Arthur Silva Sens <arthursens2005@gmail.com>

--- installer/pkg/components/cert-manager/objects.go
+++ installer/pkg/components/cert-manager/objects.go
@@ -1,11 +1,21 @@
 package certmanager
 
 import (
+	"k8s.io/apimachinery/pkg/runtime"
+
 	"github.com/gitpod-io/observability/installer/pkg/common"
 )
 
-var Objects = common.CompositeRenderFunc(
-	networkPolicy,
-	service,
-	serviceMonitor,
-)
+func Objects(ctx *common.RenderContext) common.RenderFunc {
+	if ctx.Config.Certmanager.InstallServiceMonitors {
+		return common.CompositeRenderFunc(
+			networkPolicy,
+			service,
+			serviceMonitor,
+		)
+	}
+
+	return func(cfg *common.RenderContext) ([]runtime.Object, error) {
+		return []runtime.Object{}, nil
+	}
+}
